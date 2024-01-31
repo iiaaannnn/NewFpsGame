9b6a3e722cd819be54a9f5298c8650363aa42364
iiaaannnn <212184x@mymail.nyp.edu.sg>
2024-01-31 15:34:29 +0800

    main menu

---
+++ test/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Animator mainMenuAnim;
+    public Animator settingsMenuAnim;
+
+    float waitTime = 0.01f;
+
+    //MAIN MENU
+    public void OnStartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void OnSettings()
+    {
+        mainMenuAnim.SetBool("MainMenuActive", false);
+        StartCoroutine(openSettings());
+
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+
+    //SETTINGS
+    public void OnBack()
+    {
+        settingsMenuAnim.SetBool("SettingsMenuActive", false);
+        StartCoroutine(openMainMenu());
+    }
+
+    //COROUTINES
+    IEnumerator openSettings()
+    {
+        yield return new WaitForSeconds(waitTime);
+        settingsMenuAnim.SetBool("SettingsMenuActive", true);
+
+    }
+    IEnumerator openMainMenu()
+    {
+        yield return new WaitForSeconds(waitTime);
+        mainMenuAnim.SetBool("MainMenuActive", true);
+
+    }
+}
